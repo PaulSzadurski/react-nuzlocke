579d7ce89cfa731787e29e350511ab50578241a5
Paul Szadurski <paulszadurski2@gmail.com>
2021-06-29 19:31:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 function App() {
+
+  const [pokeString, setPokeString] = useState('')
+  const [pokemon, setPokemon] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    pokeFetch();
+  }
+
+  const handlePokeInput = (event) => {
+    let pokeText = event.target.value;
+    setPokeString(pokeText)
+    console.log(pokeString);
+  }
+
+  const pokeFetch = () => {
+    const pokeURL = new URL(`https://pokeapi.co/api/v2/pokemon/${pokeString}/`)
+
+    console.log(pokeURL);
+
+    fetch(pokeURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      // setPokemon(jsonResponse.sprites.front_default);
+      let node = document.createElement("img");
+      node.src = jsonResponse.sprites.front_default;
+      document.getElementById('list').appendChild(node);
+      // console.log(pokemon)
+    })
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input placeholder="pikachu" type="text" onChange={handlePokeInput}></input>
+        <button type="submit">Submit</button>
+      </form>
+
+      <h1>
+        POKEMON
+      </h1>
+
+      <div>
+      {/* {
+        pokemon ? */}
+        <ul id="list">
+        </ul>
+        {/* // <img src={pokemon}></img>
+        :
+        <p>placeholder</p>
+      } */}
+      </div>
     </div>
   );
 }
